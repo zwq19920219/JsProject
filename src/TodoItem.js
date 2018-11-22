dcf8e71518883e8b0946c039fa66ec664e32305e
zwq19920219 <295188413@qq.com>
2018-11-22 17:17:57 +0800

    简书项目原型版本

---
+++ src/TodoItem.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class TodoItem extends React.Component{
+    render(){
+
+        return <div>
+            hello world
+        </div>
+    }
+
+}
+
+export default TodoItem;
---
+++ src/index.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import TodoItem from './TodoItem';
+
+
+ReactDOM.render(<TodoItem />, document.getElementById('root'));
+
